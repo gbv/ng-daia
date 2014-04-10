a5ac947a585fa20ad62bc976eadb9f09f7e02162
Moritz Horn <moritz.horn@gmail.com>
2014-04-10 09:34:33 +0200

    template html, default example

--- demo/app.js
+++ demo/app.js
@@ -2,5 +2,5 @@ angular.module('myApp', ['ngDAIA']);
 
 function myController($scope) {
   $scope.myAPI = "http://daia.gbv.de/";
-  $scope.myID = "opac-de-89:ppn:532043170";
+  $scope.myID = "opac-de-ma9:ppn:0685460711";
 }
