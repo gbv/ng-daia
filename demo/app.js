e070d711f1d354f8d37795132dc431e976778a8b
Jakob Voss <voss@gbv.de>
2014-05-07 10:57:16 +0200

    updated site

--- demo/app.js
+++ demo/app.js
@@ -2,12 +2,13 @@ var app = angular.module('myApp', ['ngDAIA', 'pascalprecht.translate']);
 
 app.config(['$translateProvider', function ($translateProvider) {
     $translateProvider.translations('en', {
-        INST: 'Queried institution:',
-        CAT: 'Catalogue entry',
-        DEP: 'Department:',
-        SGN: 'Shelf mark:',
-        NR: 'no records found',
-        AVB: 'Availability: ',
+        INSTITUTION: 'institution:',
+        DOCUMENT: 'document:',
+        CATALOG_ENTRY: 'catalog entry',
+        DEPARTMENT: 'department:',
+        SIGNATURE: 'shelf mark:',
+        NO_RECORDS: 'no records found',
+        AVAILABILITY: 'availability: ',
         unknown: 'unknown',
         available: 'available',
         unavailable: 'unavailable',
@@ -16,20 +17,22 @@ app.config(['$translateProvider', function ($translateProvider) {
         interloan: 'interloan',
         none: 'not available',
         expected: 'expected',
-        EXP: ' expected back:',
-        RES: 'place reservation',
-        ACC: 'access via:',
-        STATUS: 'Current status',
+        openaccess: 'open access',
+        EXPECTED_BACK: ' expected back:',
+        RESERVATION: 'place reservation',
+        ACCESS: 'access via:',
+        STATUS: 'current status',
         BUTTON_LANG_DE: 'German',
         BUTTON_LANG_EN: 'English',
     });
     $translateProvider.translations('de', {
-        INST: 'Institution:',
-        CAT: 'Katalogeintrag',
-        DEP: 'Abteilung:',
-        SGN: 'Signatur:',
-        NR: 'Keine Treffer',
-        AVB: 'Verfügbarkeit: ',
+        INSTITUTION: 'Institution:',
+        DOCUMENT: 'Dokument:',
+        CATALOG_ENTRY: 'Katalogeintrag',
+        DEPARTMENT: 'Abteilung:',
+        SIGNATURE: 'Signatur:',
+        NO_RECORDS: 'Keine Treffer',
+        AVAILABILITY: 'Verfügbarkeit: ',
         unknown: 'unbekannt',
         available: 'verfügbar',
         unavailable: 'nicht verfügbar',
@@ -38,9 +41,10 @@ app.config(['$translateProvider', function ($translateProvider) {
         interloan: 'Fernleihe',
         none: 'nicht verfügbar',
         expected: 'erwartet',
-        EXP: ' Ausgeliehen bis:',
-        RES: 'Vorkmerken',
-        ACC: 'Volltext:',
+        openaccess: 'Open Access',
+        EXPECTED_BACK: ' Ausgeliehen bis:',
+        RESERVATION: 'Vormerken',
+        ACCESS: 'Volltext:',
         STATUS: 'Aktueller Status',
         BUTTON_LANG_DE: 'Deutsch',
         BUTTON_LANG_EN: 'Englisch',
@@ -50,7 +54,7 @@ app.config(['$translateProvider', function ($translateProvider) {
 }]);
 app.controller('myController', ['$translate', '$scope', function ($translate, $scope) {
  
-    $scope.myAPI = "http://daia.gbv.de/";
+    $scope.myAPI = "//daia.gbv.de/";
     $scope.myID = "opac-de-ma9:ppn:0685460711";
     $scope.changeLanguage = function (langKey) {
         $translate.use(langKey);
