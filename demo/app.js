639e1392ef5cb2c4ae865100f7167267e02604f2
Jakob Voss <voss@gbv.de>
2016-01-27 13:42:08 +0100

    updated site

--- demo/app.js
+++ demo/app.js
@@ -1,31 +1,68 @@
 angular.module('myApp', ['ngDAIA', 'pascalprecht.translate'])
+
 .config(['$translateProvider', function ($translateProvider) {
+    
+    // load translations
     $translateProvider.useStaticFilesLoader({
-        prefix: '../src/translations/lang-',
-        suffix: '.json'
-    });
-    $translateProvider.registerAvailableLanguageKeys(['en', 'de'], {
-    'en_US':'en','en_UK':'en','de_DE':'de','de_AT':'de','de_CH':'de'
-    });
-    $translateProvider.fallbackLanguage('en');
-    $translateProvider.determinePreferredLanguage();
+        files: [{   // ng-daia
+            prefix: '../src/translations/lang-',
+            suffix: '.json'
+        },{         // this demo
+            prefix: 'translations/',
+            suffix: '.json'
+        }]
+    })
+
+    // choose a language based on browser settings
+    .registerAvailableLanguageKeys(['en', 'de'], { 'en_*': 'en', 'de_*': 'de' })
+    .fallbackLanguage('en')
+    .determinePreferredLanguage();
+
 }])
+
 .controller('myController', ['$translate', '$scope', '$http', 'ngDAIA.version',
   function ($translate, $scope, $http, version) {
     $scope.version = version;
 
+    // get and watch current language
+    $translate.onReady(function() {
+        $scope.selectedLanguage = $translate.use()
+    });
+    // see https://github.com/angular-translate/angular-translate/issues/566
+    $scope.availableLanguages = ['en','de'];
+    $scope.$watch('selectedLanguage', function(value) {
+        $translate.use(value);
+    });
+
+
     $scope.myAPI = "//daia.gbv.de/";
     $scope.myID = "opac-de-ma9:ppn:685460711";
-    $scope.changeLanguage = function (langKey) {
-        $translate.use(langKey);
-    };
+    $scope.cors = false;
 
-    // TODO: move into service
+    // TODO: move this into daia query service
+    // TODO: add patron and Authorization
     function query() {
-        $http.jsonp( $scope.myAPI, {
-            params: { id: $scope.myID, format:'json', callback:'JSON_CALLBACK' } }
-        ).success(function(response) {
+        var daiaQuery;
+        if ($scope.cors) {
+            daiaQuery = $http({
+                url: $scope.myAPI,
+                method: 'GET',
+                params: { id: $scope.myID, format: 'json' },
+                headers: {
+                    'Accept-Language': $translate.use(),
+                    // 'Authorization': access_token
+                }
+            });
+        } else {
+            // TODO: headers?
+            daiaQuery = $http.jsonp( $scope.myAPI, {
+                params: { id: $scope.myID, format: 'json', callback: 'JSON_CALLBACK' } }
+            )
+        }
+        daiaQuery.success(function(response) {
             $scope.daiaResponse = response;
+        }).error(function () {
+            $scope.daiaResponse = {};
         });
     }
 
@@ -36,6 +73,7 @@ angular.module('myApp', ['ngDAIA', 'pascalprecht.translate'])
     }
     $scope.$watch("myID",triggerQuery);
     $scope.$watch("myAPI",triggerQuery);
+    $scope.$watch("cors",triggerQuery);
 
     query();
 }]);
